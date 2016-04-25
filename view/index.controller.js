ea7a98ac9c3078011e28d4f9163d6600debd373e
Ryan Kahle <jrkahle@hotmail.com>
2016-04-25 16:15:56 -0400

    added bootstrap

--- view/index.controller.js
+++ view/index.controller.js
@@ -15,10 +15,14 @@ function mainControllerFunction(){
   var _this = this;
 
   _this.mainOptions = {
+    menu: '#menu',
     sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE'],
     navigation: true,
     navigationPosition: 'right',
-    scrollingSpeed: 1000
+    scrollingSpeed: 1000,
+    lockAnchors: true,
+    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'lastSection']
+
   };
 
 };
